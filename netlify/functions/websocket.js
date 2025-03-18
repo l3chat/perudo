8cfc3baa281047227d0e1884b05f7d039afbb97f
LeChat <l3chat@users.noreply.github.com>
2025-03-18 04:30:10 +0100

    Update websocket.js

--- netlify/functions/websocket.js
+++ netlify/functions/websocket.js
@@ -1,33 +1,11 @@
-const { Server } = require('ws');
-
-let server = null;
-let connections = [];
-
 exports.handler = async function (event, context) {
-  if (!server) {
-    server = new Server({ noServer: true });
-
-    server.on('connection', (ws) => {
-      connections.push(ws);
-      console.log("Client connected!");
-
-      ws.on('message', (message) => {
-        connections.forEach(client => {
-          if (client !== ws && client.readyState === 1) {
-            client.send(message);
-          }
-        });
-      });
-
-      ws.on('close', () => {
-        connections = connections.filter(conn => conn !== ws);
-        console.log("Client disconnected!");
-      });
-    });
-  }
-
   return {
     statusCode: 200,
-    body: "WebSocket server ready"
+    headers: {
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+      "Connection": "keep-alive"
+    },
+    body: "data: Connected to SSE server\\n\\n"
   };
 };
