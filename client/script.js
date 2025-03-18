2842a47e02e29df86838461d22244e2e1d96ca82
Oleksiy Gumenchuk <lechat.reg@gmail.com>
2025-03-18 16:16:39 +0100

    rooms

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-let playerName = prompt("Введите ваше имя") || "Игрок";
+Vlet playerName = prompt("Введите ваше имя") || "Игрок";
 let roomId = null;
 
 function logMessage(message) {
@@ -66,69 +66,6 @@ function startPolling() {
             .catch(error => console.error("Polling error:", error));
     }
     poll();
-}
-
-window.onload = function() {
-    logMessage("Добро пожаловать, " + playerName + "!");
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let playerName = prompt("Введите ваше имя") || "Игрок";
-let roomId = null;
-
-function logMessage(message) {
-    let chatBox = document.getElementById("chat-box");
-    let newMessage = document.createElement("p");
-    newMessage.textContent = message;
-    chatBox.appendChild(newMessage);
-}
-
-function createRoom() {
-    fetch("/.netlify/functions/game", {
-        method: "POST",
-        body: JSON.stringify({ type: "createRoom" }),
-        headers: { "Content-Type": "application/json" }
-    })
-    .then(response => response.json())
-    .then(data => {
-        roomId = data.roomId;
-        document.getElementById("room-selection").style.display = "none";
-        document.querySelector(".game-container").style.display = "flex";
-        document.querySelector(".controls").style.display = "block";
-        logMessage(`Вы создали комнату: ${roomId}`);
-    });
-}
-
-function joinRoom() {
-    let inputRoomId = document.getElementById("room-id-input").value;
-    if (!inputRoomId) return alert("Введите ID комнаты!");
-
-    roomId = inputRoomId;
-    fetch("/.netlify/functions/game", {
-        method: "POST",
-        body: JSON.stringify({ type: "join", roomId, player: playerName }),
-        headers: { "Content-Type": "application/json" }
-    });
-
-    document.getElementById("room-selection").style.display = "none";
-    document.querySelector(".game-container").style.display = "flex";
-    document.querySelector(".controls").style.display = "block";
-    logMessage(`Вы вошли в комнату: ${roomId}`);
 }
 
 window.onload = function() {
